f9ede8d000c2aca751c285821012875b600d6808
ArshathAkbar <140537826+ArshathAkbar@users.noreply.github.com>
2023-07-31 15:26:58 +0530

    Add files via upload

---
+++ About us.js
@@ -0,0 +1,30 @@
+// import React, { Component } from "react";
+// import './style.css';
+// class About extends Component{
+//     constructor(){
+//         super();
+//     }
+//     render(){
+//         return(
+//             <div className="bg">
+//                <h1 className="aus">Read,Enlight yourself,Worth the money</h1>
+//             </div>
+           
+//         )
+//     }
+// }
+// export default About;
+
+import React from "react";
+
+class About extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>THIS IS A SHOPPING WEBSITE</p>
+            </div>
+        );
+    }
+}
+
+export default About;
---
+++ index.js
@@ -0,0 +1,46 @@
+import ReactDOM  from "react-dom";
+i
+//import React from "react";
+//import About from "./About us";
+// import Task4 from './task4';
+// import './style.css';
+// //ReactDOM.render(<Task4/>,document.getElementById('root'));
+
+
+// class Trial{
+// method(n){
+//     var n,re,s=0;
+//     while(n)
+//     {
+//         re=n%10;
+//         s=s+re;
+//         n=Math.floor(n/10);
+//     }
+//     document.getElementById("root").innerHTML=s;
+// }
+// }
+// var obj=new Trial();
+// obj.method(1598);
+
+
+
+// import { BrowserRouter as Router, Route, Routes, Link, NavLink, Switch } from 'react-router-dom';
+// import Home from './home';
+// import About from './About us';
+// import Join from './join';
+// import React, { useRef } from 'react';
+
+// const routing = (
+//     <Router>
+//         <div>
+//             <Routes>
+//                 <Route path="/" element={<Home/>} />
+//                 <Route path="/a" element={<About/>} />
+//                 <Route path="/jo" element={<Join/>} />
+//             </Routes>
+//         </div>
+//     </Router>
+// )
+
+// ReactDOM.render(routing,document.getElementById('root'));
+
